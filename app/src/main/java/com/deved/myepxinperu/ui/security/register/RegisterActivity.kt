f7fdaabdf2e3524413decc325be122598676c943
DevEd20m <deved20m@gmail.com>
2020-05-28 23:42:03 -0500

    added view of RegisterActivity

--- app/src/main/java/com/deved/myepxinperu/ui/security/register/RegisterActivity.kt
+++ app/src/main/java/com/deved/myepxinperu/ui/security/register/RegisterActivity.kt
@@ -1,13 +1,32 @@
 package com.deved.myepxinperu.ui.security.register
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.deved.myepxinperu.R
+import com.deved.myepxinperu.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
-
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var viewmodel :RegisterViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val view = super.onCreateView(name, context, attrs)
+        binding.materialButtonRegister.setOnClickListener(this)
+        return view
+    }
+
+    override fun onClick(button: View?) {
+        when (button?.id) {
+            R.id.materialButtonRegister -> {
+            }
+        }
     }
 }
