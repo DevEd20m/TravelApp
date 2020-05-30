07c7d81edc40cc5db6ee2412fd5a1d2677398637
DevEd20m <deved20m@gmail.com>
2020-05-30 00:51:23 -0500

    config UiContext for getString's

--- app/src/main/java/com/deved/myepxinperu/MyExpInPeruApplication.kt
+++ app/src/main/java/com/deved/myepxinperu/MyExpInPeruApplication.kt
@@ -1,7 +1,11 @@
 package com.deved.myepxinperu
 
 import android.app.Application
+import com.deved.myepxinperu.ui.common.UiContext
 
 class MyExpInPeruApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        UiContext.init(applicationContext)
+    }
 }
