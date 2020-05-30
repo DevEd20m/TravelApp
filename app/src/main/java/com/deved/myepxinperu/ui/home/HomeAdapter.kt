0a257228b4445cb56668d1d22bffbba73d5ca065
DevEd20m <deved20m@gmail.com>
2020-05-30 00:52:40 -0500

    implemented extension for inflateLayout

--- app/src/main/java/com/deved/myepxinperu/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/deved/myepxinperu/ui/home/HomeAdapter.kt
@@ -1,6 +1,5 @@
 package com.deved.myepxinperu.ui.home
 
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +8,7 @@ import com.deved.domain.Places
 import com.deved.myepxinperu.R
 import com.deved.myepxinperu.databinding.ItemPlacesBinding
 import com.deved.myepxinperu.ui.common.basicDiffUtil
+import com.deved.myepxinperu.ui.common.inflate
 
 class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
 
@@ -17,9 +17,7 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_places, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(parent.inflate(R.layout.item_places,false))
     }
 
     override fun getItemCount(): Int = places.size
